5c7d70fa1b6b258127e7b0643401a1838d5ceec0
Rohith2510 <rohithr2510@gmail.com>
2024-06-19 12:12:46 -0400

    Reapply "Merge branch 'Developing' into Rohith"
    This reverts commit ffc879cf9db6469df80686b0e69960002f9f7b6a.

--- Assets/Game/Scripts/Inventory/InventoryItem.cs
+++ Assets/Game/Scripts/Inventory/InventoryItem.cs
@@ -2,39 +2,60 @@ using Game.Scripts.Interactable;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
-public abstract class InventoryItem : MonoBehaviour
+[RequireComponent(typeof(SphereCollider))]
+public abstract class InventoryItem : MonoBehaviour, IInteractable
 {
-    public string ItemId;
-    public string itemName;
-    public Sprite itemIcon;
-    public GameObject itemPreview;
-    public int btnIndex;
+    public string itemName; //Name of Item
+    public Sprite itemIcon; //Icon to show on button
+    public GameObject itemPreview; //Mesh to be shown on Inventory Cam
+    public string itemDescription; //Mesh to be shown on Inventory Cam
+    public GameObject interactPopup; //Popup to show when Player is Close
+    //public string interactDescription;    //Can be enabled to have dynamic messages (As of now assuming to be in Prefab UI Itself)
 
-    private GameManager gameManager;
 
-    private void Awake()
+    public bool bInteractable = true; //Make it false when already interacted with
+
+    protected InventoryHandler inventoryHandler;    //Just a local reference of Inventory System (just to avoid writing the whole thing)
+   
+    #region Unused but needed stuff (thanks to Interface)
+    [HideInInspector] public InputAction Action => throw new System.NotImplementedException();
+    [HideInInspector] public int Priority => throw new System.NotImplementedException();
+    #endregion
+
+    private void Start()
     {
-        gameManager = FindObjectOfType<GameManager>();
-    }
+        inventoryHandler = InventoryHandler.Instance;
+        GetComponent<Collider>().isTrigger = true;
 
-    public void Interact()
-    {
-        if (gameManager != null)
-        {
-            if (CompareTag("KeyItem"))
-            {
-                gameManager.AddKeyItem(gameObject.name);
-            }
-            else if (CompareTag("ResourceItem"))
-            {
-                gameManager.AddResourceItem(gameObject.name, 1); // Adjust amount as needed
-            }
-
-            Destroy(gameObject); // Remove the item from the scene
+        //No triggers for stuff that is not interactable
+        if(!bInteractable) 
+        { 
+            GetComponent<Collider>().enabled = false;
         }
     }
 
-    public abstract void Use();
-    public abstract string GetDescription();
+    protected void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.tag == "Player")
+        {
+            GameManager.Instance.playerRef.SetInteractable(this);
+        }
+    }
+
+    protected void OnTriggerExit(Collider other)
+    {
+
+        if (other.tag == "Player")
+        {
+            GameManager.Instance.playerRef.SetInteractable(null);
+        }
+    }
+
+    public virtual void Interact(CharacterBase player)
+    {
+        //Default Base Method (Will be overrided in other sub classes)
+    }
 }
---
+++ Assets/Game/Scripts/Objects/UsableObject.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// @Sami 06/12/24
+/// This is the class for any usable object like Gloves / Etc
+/// </summary>
+
+public class UsableObject : InventoryItem
+{
+    [Tooltip("Whether the item should be removed after use")]
+    public bool bShouldBeRemoved = false;
+
+    public override void Interact(CharacterBase player)
+    {
+        if(inventoryHandler != null) 
+        {
+            inventoryHandler.AddUsable(this);
+        }
+    }
+
+    public virtual void Use()
+    {
+        // Implement general use logic for object items
+        Debug.Log("Using object item: " + itemName);
+    }
+}
